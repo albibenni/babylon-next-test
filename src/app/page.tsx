1a9b3b30ef571b7949f4413f132852f70c928c4b
Alberto Benatti <albi.benni8@gmail.com>
2023-10-16 16:49:41 +0200

    dmg helmet working somehow - paths is weird in import mesh

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,47 +1,66 @@
+"use client";
 import {
   Engine,
   Scene,
   Vector3,
   SceneLoader,
   ArcRotateCamera,
+  FreeCamera,
 } from "@babylonjs/core";
 import "@babylonjs/loaders/glTF/2.0";
 import "@babylonjs/core/Helpers/sceneHelpers";
+import { useEffect, useRef } from "react";
 
 export default function Home() {
-  const canvas = document.getElementById("renderCanvas");
-  if (!canvas) throw new Error("Canvas not found");
-  const engine = new Engine(canvas as HTMLCanvasElement, true);
+  const reactCanvas = useRef<HTMLCanvasElement>(null);
 
-  const scene = new Scene(engine);
-  const camera = new ArcRotateCamera("camera", 1, 1, 4, new Vector3(0, 0, 0));
-  camera.attachControl(canvas, true);
+  useEffect(() => {
+    const { current: canvas } = reactCanvas;
+    if (!canvas) return;
 
-  scene.createDefaultEnvironment({
-    environmentTexture: "./public/forest",
-    skyboxTexture: "./public/forest",
-  });
+    const engine = new Engine(canvas, true);
 
-  engine.runRenderLoop(function () {
-    scene.render();
-  });
-  window.addEventListener("resize", () => {
-    engine.resize();
-  });
+    const scene = new Scene(engine);
+    const camera = new FreeCamera("camera", new Vector3(0, 0.75, -2), scene);
+    camera.attachControl(reactCanvas, true);
 
-  SceneLoader.ImportMeshAsync("", "./public", "pc.gltf", scene, (meshes) => {
-    console.log("Mesh ", meshes);
-  }).then((helmet) => {
-    helmet.meshes[0]!.position.y = 2;
-    camera.setTarget(helmet.meshes[0]!);
+    // scene.createDefaultEnvironment({
+    //   environmentTexture: "./forest",
+    //   skyboxTexture: "./forest",
+    // });
 
-    scene.onBeforeRenderObservable.add(() => {
-      helmet.meshes[0]!.rotate(new Vector3(0, 1, 0), 0.001);
+    engine.runRenderLoop(function () {
+      scene.render();
     });
+    window.addEventListener("resize", () => {
+      engine.resize();
+    });
+
+    // SceneLoader.ImportMeshAsync("", "./public/", "pc.gltf", scene, (meshes) => {
+    //   console.log("Mesh ", meshes);
+    // })
+
+    SceneLoader.ImportMeshAsync(
+      "",
+      "./",
+      "damagedHelmet.glb",
+      scene,
+      (meshes) => {
+        console.log("Mesh ", meshes);
+      }
+    );
+    // .then((helmet) => {
+    //   helmet.meshes[0]!.position.y = 2;
+    //   camera.setTarget(helmet.meshes[0]!);
+
+    //   scene.onBeforeRenderObservable.add(() => {
+    //     helmet.meshes[0]!.rotate(new Vector3(0, 1, 0), 0.001);
+    //   });
+    // });
   });
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <canvas />
+    <main className="flex w-full flex-col items-center justify-between p-24">
+      <canvas className="w-full h-full" id="babylon-canvas" ref={reactCanvas} />
     </main>
   );
 }
