bc96590cb4ace7a4d65867bfc629d79a418bf4df
Alberto Benatti <albi.benni8@gmail.com>
2023-10-17 10:56:40 +0200

    typo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
     <main className="flex w-full flex-col items-center justify-between p-24">
-      <h1 className="text-4xl font-bold">Welcome to Babylon vs Tree preview</h1>
+      <h1 className="text-4xl font-bold">
+        Welcome to Babylon.js vs Tree.js preview
+      </h1>
       <p className="text-xl">
         Tree.js preview: <a href="/tree-js">~/tree-js</a>
       </p>
