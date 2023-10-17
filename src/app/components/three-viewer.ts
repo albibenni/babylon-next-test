493166d7c1dd83bd8100b2a133089db66b9981a8
Alberto Benatti <albi.benni8@gmail.com>
2023-10-17 21:02:08 +0200

    three working - fix rotation

--- src/app/components/three-viewer.ts
+++ src/app/components/three-viewer.ts
@@ -1,39 +1,21 @@
 "use client";
 import * as THREE from "three";
-import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+import { GLTF, GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+import SceneInit from "../three/SceneInit";
 
-export const viewer = (canvas: HTMLCanvasElement) => {
-  // const engine = new Engine(canvas, true);
-
-  // const scene = new Scene(engine);
-  const scene = new THREE.Scene();
+export const viewer = (canvas: SceneInit) => {
   const loader = new GLTFLoader();
 
-  // const camera = new ArcRotateCamera("camera", 1, 1, 4, new Vector3(0, 0, 0));
-  const camera = new THREE.PerspectiveCamera(
-    75,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
-  );
-  camera.position.z = 96;
-
-  const renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  document.body.appendChild(renderer.domElement);
-
-  const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
-  ambientLight.castShadow = true;
-  scene.add(ambientLight);
-
-  const spotLight = new THREE.SpotLight(0xffffff, 0.5);
-  spotLight.castShadow = true;
-  scene.add(spotLight);
-  scene.add();
+  // LOAD GLTF
+  let loadedModel: GLTF;
   loader.load(
-    "scene.gltf",
+    "./bike/scene.gltf",
     (gltf) => {
-      scene.add(gltf.scene);
+      loadedModel = gltf;
+      gltf.scene.rotation.y = Math.PI / 8;
+      gltf.scene.position.y = 3;
+      gltf.scene.scale.set(10, 10, 10);
+      canvas.scene!.add(gltf.scene);
     },
     undefined,
     function (error) {
@@ -41,7 +23,12 @@ export const viewer = (canvas: HTMLCanvasElement) => {
     }
   );
   const animate = () => {
-    renderer.render(scene, camera);
-    window.requestAnimationFrame(animate);
+    if (loadedModel) {
+      loadedModel.scene.rotation.x += 0.01;
+      loadedModel.scene.rotation.y += 0.01;
+      loadedModel.scene.rotation.z += 0.01;
+    }
+    requestAnimationFrame(animate);
   };
+  animate();
 };
--- src/app/three/page.tsx
+++ src/app/three/page.tsx
@@ -1,13 +1,16 @@
 "use client";
 import { useEffect, useRef } from "react";
 import { viewer } from "../components/three-viewer";
+import SceneInit from "./SceneInit";
 
 export default function Home() {
   const reactCanvas = useRef<HTMLCanvasElement>(null);
 
   useEffect(() => {
-    const { current: canvas } = reactCanvas;
-    if (!canvas) return;
+    // const { current: canvas } = reactCanvas;
+    const canvas = new SceneInit("three-canvas");
+    canvas.initialize();
+    canvas.animate();
     viewer(canvas);
   }, []);
   return (
