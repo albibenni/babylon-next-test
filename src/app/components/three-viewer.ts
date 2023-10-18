df80284eb1958535766abceb6a143b2a0af53b36
Alberto Benatti <albi.benni8@gmail.com>
2023-10-18 11:47:13 +0200

    wip scene with shadow

--- src/app/components/three-viewer.ts
+++ src/app/components/three-viewer.ts
@@ -2,9 +2,11 @@
 import * as THREE from "three";
 import { GLTF, GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 import SceneInit from "../three/SceneInit";
+import { RGBELoader } from "three/examples/jsm/loaders/RGBELoader.js";
 
 export const viewer = (canvas: SceneInit) => {
   const loader = new GLTFLoader();
+  const rgbeloader = new RGBELoader();
 
   // LOAD GLTF
   let loadedModel: GLTF;
@@ -12,9 +14,10 @@ export const viewer = (canvas: SceneInit) => {
     "./bike/scene.gltf",
     (gltf) => {
       loadedModel = gltf;
-      gltf.scene.rotation.y = Math.PI / 8;
       gltf.scene.position.y = 3;
-      gltf.scene.scale.set(10, 10, 10);
+      gltf.scene.scale.set(15, 15, 15);
+      gltf.scene.castShadow = true;
+      gltf.scene.receiveShadow = false;
       canvas.scene!.add(gltf.scene);
     },
     undefined,
@@ -22,6 +25,23 @@ export const viewer = (canvas: SceneInit) => {
       console.error(error);
     }
   );
+
+  const planeGeometry = new THREE.PlaneGeometry(20, 20, 32, 32);
+  const planeMaterial = new THREE.MeshStandardMaterial({ color: 0x00ff00 });
+  const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+  plane.receiveShadow = true;
+  plane.rotation.x = -Math.PI / 2;
+  canvas.scene!.add(plane);
+
+  // rgbeloader.load(
+  //   "./model/texture/venice_sunset_1k.hdr",
+  //   (texture: THREE.Texture | THREE.CubeTexture | null) => {
+  //     texture!.mapping = THREE.EquirectangularReflectionMapping;
+
+  //     canvas.scene!.background = texture;
+  //     canvas.scene!.environment = texture;
+  //   }
+  // );
   const animate = () => {
     if (loadedModel) {
       loadedModel.scene.rotation.y += 0.002;
